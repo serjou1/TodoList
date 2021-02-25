957d026faa1fea46ff7d4051d27bb6eeb1d1f253
sergnik97 <sergnik97@gmail.com>
2021-02-25 09:32:02 +0200

    added repositories

---
+++ TodoList.DAL/Repositories/TaskRepository.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.DAL.Interfaces;
+using TodoList.DAL.Models;
+
+namespace TodoList.DAL.Repositories
+{
+    public class TaskRepository : IRepository<TaskDal>
+    {
+        private readonly TodoListContext _context;
+
+        public TaskRepository(TodoListContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(TaskDal task)
+        {
+            _context.Tasks.Add(task);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                // todo handle this
+                //throw new
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var task = new TaskDal { Id = id };
+
+            _context.Tasks.Attach(task);
+            _context.Tasks.Remove(task);
+
+            try
+            {
+                var numberOfEntities = await _context.SaveChangesAsync();
+                // todo if 0
+                //if (numberOfEntities == 0)
+                //    throw new 
+            }
+            catch (DbUpdateException e)
+            {
+                // todo handle this
+                //throw new
+            }
+        }
+
+        public async Task<IEnumerable<TaskDal>> FindAsync(Expression<Func<TaskDal, bool>> predicate)
+        {
+            return await _context.Tasks.Where(predicate).ToArrayAsync();
+        }
+
+        public async Task<IEnumerable<TaskDal>> GetAllAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskDal> GetAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);// ?? new 
+
+            // todo if user null
+
+        }
+
+        public async Task UpdateAsync(TaskDal task)
+        {
+            _context.Entry(task).State = System.Data.Entity.EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                // todo handle
+            }
+        }
+    }
+}
---
+++ TodoList.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoList.DAL.Interfaces;
+using TodoList.DAL.Models;
+
+namespace TodoList.DAL.Repositories
+{
+    public class UserRepository : IRepository<UserDal>
+    {
+        private readonly TodoListContext _context;
+
+        public UserRepository(TodoListContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(UserDal user)
+        {
+            _context.Users.Add(user);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                // todo handle this
+                //throw new
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = new UserDal { Id = id };
+
+            _context.Users.Attach(user);
+            _context.Users.Remove(user);
+
+            try
+            {
+                var numberOfEntities = await _context.SaveChangesAsync();
+                // todo if 0
+                //if (numberOfEntities == 0)
+                //    throw new 
+            }
+            catch (DbUpdateException e)
+            {
+                // todo handle this
+                //throw new
+            }
+        }
+
+        public async Task<IEnumerable<UserDal>> FindAsync(Expression<Func<UserDal, bool>> predicate)
+        {
+            return await _context.Users.Where(predicate).ToArrayAsync();
+        }
+
+        public async Task<IEnumerable<UserDal>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<UserDal> GetAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);// ?? new 
+
+            // todo if user null
+
+        }
+
+        public async Task UpdateAsync(UserDal user)
+        {
+            _context.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch
+            {
+                // todo handle
+            }
+        }
+    }
+}
